1aee9bb0a2ed3563bc4c92fe20be0b695303642b
JoakimHoegaas <62018628+JoakimHoegaas@users.noreply.github.com>
2021-03-17 16:39:25 +0100

    Initial commit

---
+++ CountAndSum/Calc.cs
@@ -0,0 +1,32 @@
+﻿
+namespace CountAndSum
+{
+    public class Calc
+    {
+        private readonly int[] _integers;
+
+        public Calc(int[] integers)
+        {
+            _integers = integers;
+        }
+
+        public int[] CalcSum()
+        {
+            int x = 0;
+            int y = 0;
+            foreach(var n in _integers)
+            {
+                if (n > 0)
+                {
+                    x += 1;
+                }
+                else
+                {
+                    y -= n;
+                }
+            }
+
+            return new int[] { x, y };
+        }
+    }
+}
---
+++ CountAndSum/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CountAndSum
+{
+    class Program
+    {
+        // https://edabit.com/challenge/SXeEZPxDM9Y5HzLvw
+        // SOLVED
+
+        static void Main(string[] args)
+        {
+            var integer = new Calc(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -10, -15 });
+            var x = integer.CalcSum();
+            foreach (var n in x) Console.WriteLine(n);
+        }
+    }
+}
